7a0b0b2a6c0324b04ddba7e39d46034c7a32afad
smack0007 <zachary.snow@gmail.com>
2020-11-10 10:59:42 +0100

    Added require.js

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Junkyard
-A collection of random code that I wrote but never got used for whatever reason.
+# smack0007's Junkyard
+A collection of random code that I wrote but never got used for whatever reason. None of the code
+here is guarenteed to work or to have ever worked. Use at your own risk.
---
+++ js/require.js
@@ -0,0 +1,31 @@
+﻿const modules = {};
+async function require(path, currentPath) {
+    let currentDirectory = "./";
+    if (currentPath !== undefined) {
+        currentDirectory = currentPath.substring(0, currentPath.lastIndexOf("/") + 1);
+    }
+
+    let pathToLoad = path;
+    if (pathToLoad.startsWith("./")) {
+        pathToLoad = currentDirectory + pathToLoad.substring("./".length);
+    }
+
+    if (!pathToLoad.endsWith(".js")) {
+        pathToLoad += ".js";
+    }
+
+    // console.info("require", path, currentPath, currentDirectory, pathToLoad);
+
+    if (modules[pathToLoad] === undefined) {
+        const module = { exports: {} };
+        modules[pathToLoad] = module;
+
+        const src = await fetch(pathToLoad).then(response => response.text());
+        // console.info(src);
+
+        const loader = Function("require, exports, module", src);
+        loader(function (path) { return require(path, pathToLoad); }, module.exports, module);
+    }
+
+    return modules[pathToLoad].exports;
+}
